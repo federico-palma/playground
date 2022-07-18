5ff99e9246099071a3aa8782f45ea256e1e2be37
federico-palma <fedepal9@gmail.com>
2022-07-18 12:05:02 +0200

    Add React Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
+import EtchASketchPage from "./Pages/Etch-a-SketchPage/Etch-a-SketchPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/Etch-a-Sketch' exact>
+          <EtchASketchPage />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
