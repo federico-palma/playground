24449062e0bd27340d080bef0e58ed5308046f07
federico-palma <fedepal9@gmail.com>
2022-07-23 16:27:53 +0200

    Add Screen def and onMouseEnter event

--- src/Pages/Etch-a-SketchPage/Components/ScreenComponent.js
+++ src/Pages/Etch-a-SketchPage/Components/ScreenComponent.js
@@ -6,12 +6,12 @@ const ScreenComponent = () => {
 
   return (
     <div className={classes.screen}>
-      <div className={classes["grid-container"]}>
-        {[...Array(screenDefinition)].map((n, i) => {
+      <div className={classes["grid-container"]} style={{gridTemplateRows: `repeat(${screenDefinition}, 1fr)`, gridTemplateColumns: `repeat(${screenDefinition}, 1fr)`}}>
+        {[...Array(screenDefinition * screenDefinition)].map((n, i) => {
           return (
             <div
               className={classes["grid-cell"]}
-              onMouseEnter={console.log(i)}
+              onMouseEnter={() => console.log(i)}
               key={i}
             ></div>
           );
