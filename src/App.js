5c937998112f10aa0cf23a9cfca3f4ae6d13f685
federico-palma <fedepal9@gmail.com>
2022-07-25 16:00:12 +0200

    Add title to tab

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import EtchASketchPage from "./Pages/Etch-a-SketchPage/Etch-a-SketchPage";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "PlayGround"
+  }, [])
+
   return (
     <div className="App">
       <Switch>
