43e99d6326635a1e5a47a0b7fb83f64b02488bbf
federico-palma <fedepal9@gmail.com>
2022-07-27 10:37:39 +0200

    Add styling to homepage

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="card-list">
-      <Link to="/Etch-a-Sketch">
-        <Card title="Etch-a-Sketch" imagePath="" />
-      </Link>
-      <Card title="Tic-Tac-Toe" imagePath="" />
-      <Card title="Calculator" imagePath="" />
+    <div className="homepage-container">
+      <div className="card-list">
+        <Link to="/Etch-a-Sketch">
+          <Card title="Etch-a-Sketch" imagePath="" />
+        </Link>
+        <Card title="Tic-Tac-Toe" imagePath="" />
+        <Card title="Calculator" imagePath="" />
+      </div>
     </div>
   );
 };
