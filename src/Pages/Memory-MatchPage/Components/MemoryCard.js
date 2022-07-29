71c5c5b22ea7a89f56aefd8a30197ab9c28bfa81
federico-palma <fedepal9@gmail.com>
2022-07-29 09:42:49 +0200

    Add event for turning card around

--- src/Pages/Memory-MatchPage/Components/MemoryCard.js
+++ src/Pages/Memory-MatchPage/Components/MemoryCard.js
@@ -2,8 +2,8 @@ import classes from "../Memory-Match.module.css";
 
 const MemoryCard = (props) => {
   return (
-    <div className={classes["memory-card"]}>
-      <h1>{ props.symbol }</h1>
+    <div className={classes["memory-card"]} onClick={() => props.handleTurnCard(props.index)}>
+      <h1 className={props.isTurned ? classes.hidden : ''}>{ props.symbol }</h1>
     </div>
   );
 };
--- src/Pages/Memory-MatchPage/Memory-MatchPage.js
+++ src/Pages/Memory-MatchPage/Memory-MatchPage.js
@@ -1,39 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MemoryCard from "./Components/MemoryCard";
 import classes from "./Memory-Match.module.css";
-
-const cardArray = [
-  { symbol: 1, isTurned: false },
-  { symbol: 1, isTurned: false },
-  { symbol: 2, isTurned: false },
-  { symbol: 2, isTurned: false },
-  { symbol: 3, isTurned: false },
-  { symbol: 3, isTurned: false },
-  { symbol: 4, isTurned: false },
-  { symbol: 4, isTurned: false },
-  { symbol: 5, isTurned: false },
-  { symbol: 5, isTurned: false },
-  { symbol: 6, isTurned: false },
-  { symbol: 6, isTurned: false },
-  { symbol: 7, isTurned: false },
-  { symbol: 7, isTurned: false },
-  { symbol: 8, isTurned: false },
-  { symbol: 8, isTurned: false },
-  { symbol: 9, isTurned: false },
-  { symbol: 9, isTurned: false },
-  { symbol: 10, isTurned: false },
-  { symbol: 10, isTurned: false },
-];
 
 const MemoryMatchPage = () => {
   useEffect(() => {
     document.title = "PlayGround | Memory-Match";
   }, []);
+  
+  const [cardArrayState, setCardArrayState] = useState([
+    { symbol: 1, isTurned: false },
+    { symbol: 1, isTurned: false },
+    { symbol: 2, isTurned: false },
+    { symbol: 2, isTurned: false },
+    { symbol: 3, isTurned: false },
+    { symbol: 3, isTurned: false },
+    { symbol: 4, isTurned: false },
+    { symbol: 4, isTurned: false },
+    { symbol: 5, isTurned: false },
+    { symbol: 5, isTurned: false },
+    { symbol: 6, isTurned: false },
+    { symbol: 6, isTurned: false },
+    { symbol: 7, isTurned: false },
+    { symbol: 7, isTurned: false },
+    { symbol: 8, isTurned: false },
+    { symbol: 8, isTurned: false },
+  ]);
+
+  //   const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleTurnCard = (index) => {
+    let tempArray = cardArrayState;
+    tempArray[index].isTurned = !tempArray[index].isTurned;
+    setCardArrayState([...tempArray]);
+  };
+
   return (
     <div className={classes.background}>
       <main className={classes["card-table"]}>
-        {cardArray.map((elem, i) => {
-          return <MemoryCard symbol={elem.symbol} isTurned={elem.isTurned} key={i}/>;
+        {cardArrayState.map((elem, i) => {
+          return (
+            <MemoryCard
+              symbol={elem.symbol}
+              isTurned={elem.isTurned}
+              key={i}
+              index={i}
+              handleTurnCard={handleTurnCard}
+            />
+          );
         })}
       </main>
     </div>
