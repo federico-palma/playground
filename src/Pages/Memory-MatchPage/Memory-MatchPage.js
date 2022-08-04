2d452be56f649531fb32a10b2c04aab20fa6db49
federico-palma <fedepal9@gmail.com>
2022-08-04 12:11:57 +0200

    Change symbols of cards

--- src/Pages/Memory-MatchPage/Memory-MatchPage.js
+++ src/Pages/Memory-MatchPage/Memory-MatchPage.js
@@ -9,29 +9,29 @@ const MemoryMatchPage = () => {
   }, []);
 
   const [cardArrayState, setCardArrayState] = useState([
-    { id: "1a", symbol: 1, isTurned: false, isMatched: false },
-    { id: "1b", symbol: 1, isTurned: false, isMatched: false },
-    { id: "2a", symbol: 2, isTurned: false, isMatched: false },
-    { id: "2b", symbol: 2, isTurned: false, isMatched: false },
-    { id: "3a", symbol: 3, isTurned: false, isMatched: false },
-    { id: "3b", symbol: 3, isTurned: false, isMatched: false },
-    { id: "4a", symbol: 4, isTurned: false, isMatched: false },
-    { id: "4b", symbol: 4, isTurned: false, isMatched: false },
-    { id: "5a", symbol: 5, isTurned: false, isMatched: false },
-    { id: "5b", symbol: 5, isTurned: false, isMatched: false },
-    { id: "6a", symbol: 6, isTurned: false, isMatched: false },
-    { id: "6b", symbol: 6, isTurned: false, isMatched: false },
-    { id: "7a", symbol: 7, isTurned: false, isMatched: false },
-    { id: "7b", symbol: 7, isTurned: false, isMatched: false },
-    { id: "8a", symbol: 8, isTurned: false, isMatched: false },
-    { id: "8b", symbol: 8, isTurned: false, isMatched: false },
+    { id: "1a", symbol: "\u03A9", isTurned: false, isMatched: false },
+    { id: "1b", symbol: "\u03A9", isTurned: false, isMatched: false },
+    { id: "2a", symbol: "\u262F", isTurned: false, isMatched: false },
+    { id: "2b", symbol: "\u262F", isTurned: false, isMatched: false },
+    { id: "3a", symbol: "\u25B2", isTurned: false, isMatched: false },
+    { id: "3b", symbol: "\u25B2", isTurned: false, isMatched: false },
+    { id: "4a", symbol: "\u2663", isTurned: false, isMatched: false },
+    { id: "4b", symbol: "\u2663", isTurned: false, isMatched: false },
+    { id: "5a", symbol: "\u2605", isTurned: false, isMatched: false },
+    { id: "5b", symbol: "\u2605", isTurned: false, isMatched: false },
+    { id: "6a", symbol: "\u058E", isTurned: false, isMatched: false },
+    { id: "6b", symbol: "\u058E", isTurned: false, isMatched: false },
+    { id: "7a", symbol: "\u2388", isTurned: false, isMatched: false },
+    { id: "7b", symbol: "\u2388", isTurned: false, isMatched: false },
+    { id: "8a", symbol: "\u23FE", isTurned: false, isMatched: false },
+    { id: "8b", symbol: "\u23FE", isTurned: false, isMatched: false },
   ]);
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disableCards, setDisableCards] = useState(false)
-  const [turnNum, setTurnNum] = useState(0)
+  const [disableCards, setDisableCards] = useState(false);
+  const [turnNum, setTurnNum] = useState(0);
 
   const handleIsPlaying = () => {
     setIsPlaying(true);
@@ -47,7 +47,7 @@ const MemoryMatchPage = () => {
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
-      setDisableCards(true)
+      setDisableCards(true);
       if (choiceOne.symbol === choiceTwo.symbol) {
         setCardArrayState((preVal) => {
           return preVal.map((card) => {
@@ -58,7 +58,7 @@ const MemoryMatchPage = () => {
             }
           });
         });
-        resetTurn()
+        resetTurn();
       } else {
         setTimeout(() => resetTurn(), 1500);
       }
@@ -80,24 +80,24 @@ const MemoryMatchPage = () => {
       });
     });
   };
-  
+
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
-    setDisableCards(false)
-    setTurnNum(preVal => preVal + 1)
+    setDisableCards(false);
+    setTurnNum((preVal) => preVal + 1);
   };
 
   const startGame = () => {
-    setTurnNum(0)
+    setTurnNum(0);
     turnAllCards();
     shuffleCards();
   };
 
   return (
     <div className={classes.background}>
-        {!isPlaying && <StartGameModal handleIsPlaying={handleIsPlaying} />}
-        <h2 className={classes["turn-counter"]}>Turn number: {turnNum}</h2>
+      {!isPlaying && <StartGameModal handleIsPlaying={handleIsPlaying} />}
+      <h2 className={classes["turn-counter"]}>Turn number: {turnNum}</h2>
       <main className={classes["card-table"]}>
         {cardArrayState.map((card) => {
           return (
