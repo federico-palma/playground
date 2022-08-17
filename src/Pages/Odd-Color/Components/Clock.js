ba4809953f2594ee0bcba985aa1c0367fb6f12a9
federico-palma <fedepal9@gmail.com>
2022-08-17 17:55:38 +0200

    Prettier

--- src/Pages/Odd-Color/Components/Clock.js
+++ src/Pages/Odd-Color/Components/Clock.js
@@ -2,19 +2,17 @@ import { useEffect } from "react";
 import classes from "../Odd-Color.module.css";
 
 const Clock = (props) => {
-  const { handleTimer } = props
+  const { handleTimer } = props;
 
   useEffect(() => {
     let countdownTimeout;
     if (props.isPlaying && props.countdown > 0) {
       countdownTimeout = setTimeout(() => {
-        handleTimer()
+        handleTimer();
       }, 1000);
     }
     return () => clearTimeout(countdownTimeout);
   }, [props.countdown, props.isPlaying, handleTimer]);
-
-
 
   return <div className={classes.clock}>{props.countdown}</div>;
 };
