6d2f654ab17e473cae45f8d4f89a223d904f1ad3
federico-palma <fedepal9@gmail.com>
2022-08-22 17:31:23 +0200

    Create basic layout for simon says

--- src/Pages/Simon-says/SimonSaysPage.js
+++ src/Pages/Simon-says/SimonSaysPage.js
@@ -1,5 +1,16 @@
+import classes from "./SimonSays.module.css";
+
 const SimonSaysPage = () => {
-  return <div>Tesetttt</div>;
+  return (
+    <div className={classes.background}>
+      <main className={classes.board}>
+        <div className={`${classes.btn} ${classes["btn-1"]}`}></div>
+        <div className={`${classes.btn} ${classes["btn-2"]}`}></div>
+        <div className={`${classes.btn} ${classes["btn-3"]}`}></div>
+        <div className={`${classes.btn} ${classes["btn-4"]}`}></div>
+      </main>
+    </div>
+  );
 };
 
 export default SimonSaysPage;
