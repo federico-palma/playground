369cff60a36db65dd219d9b1ef1946a67cddd227
federico-palma <fedepal9@gmail.com>
2022-08-26 16:42:41 +0200

    Create show sequence functionality

--- src/Pages/Simon-says/SimonSaysPage.js
+++ src/Pages/Simon-says/SimonSaysPage.js
@@ -1,13 +1,73 @@
+import { useState, useCallback } from "react";
+import Board from "./Components/Board";
 import classes from "./SimonSays.module.css";
 
 const SimonSaysPage = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [btnsRefs, setBtnsRefs] = useState([]);
+  const [disableBtns, setDisableBtns] = useState(false);
+  const [sequenceArray, setSequenceArray] = useState([]);
+  const [playerChoiceArray, setPlayerChoiceArray] = useState([]);
+  const [playerTurn, setPlayerTurn] = useState(false);
+
+  const setRefsHandler = useCallback((a, b, c, d) => {
+    setBtnsRefs([a, b, c, d]);
+  }, []);
+
+  const getRandomNum = () => {
+    return Math.floor(Math.random() * 3);
+  };
+
+  const playSequence = () => {
+    console.log("Starting seq");
+    let index = 0;
+    const clearFilter = currentIndex => {
+      btnsRefs[currentIndex - 1].current.style.filter = "contrast(100%)";
+    };
+
+    const sequenceInterval = setInterval(() => {
+      if (index === sequenceArray.length - 1) {
+        clearInterval(sequenceInterval);
+      }
+
+      btnsRefs[sequenceArray[index]].current.style.filter = "contrast(400%)";
+      
+      setTimeout(() => {
+        clearFilter(index);
+      }, 500);
+
+      index += 1;
+    }, 1500);
+  };
+
+  const newRound = () => {
+    console.log("New Round");
+    setPlayerTurn(false);
+    setPlayerChoiceArray([]);
+    setSequenceArray(prevState => [...(prevState + getRandomNum())]);
+    playSequence();
+  };
+
+  const addPlayerChoiceHandler = btnIndex => {
+    if (isPlaying && playerTurn) {
+      setPlayerChoiceArray(prevState => [...(prevState + btnIndex)]);
+    }
+  };
+
+  const startGame = () => {
+    console.log("Starting game");
+    setIsPlaying(true);
+    setSequenceArray([]);
+    newRound();
+  };
+
   return (
     <div className={classes.background}>
+      <button onClick={startGame}>Start Game</button>
       <main className={classes.board}>
-        <div className={`${classes.btn} ${classes["btn-1"]}`}></div>
-        <div className={`${classes.btn} ${classes["btn-2"]}`}></div>
-        <div className={`${classes.btn} ${classes["btn-3"]}`}></div>
-        <div className={`${classes.btn} ${classes["btn-4"]}`}></div>
+        <Board
+          addPlayerChoiceHandler={addPlayerChoiceHandler}
+          setRefsHandler={setRefsHandler}></Board>
       </main>
     </div>
   );
